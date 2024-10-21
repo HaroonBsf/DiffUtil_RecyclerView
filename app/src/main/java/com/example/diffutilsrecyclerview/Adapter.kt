f9782f89f8984332367cce473d341c819b80edad
HaroonBsf <haroonbsf786@gmail.com>
2024-10-21 17:16:44 +0500

    Detailed Activity

--- app/src/main/java/com/example/diffutilsrecyclerview/Adapter.kt
+++ app/src/main/java/com/example/diffutilsrecyclerview/Adapter.kt
@@ -1,6 +1,7 @@
 package com.example.diffutilsrecyclerview
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -38,7 +39,9 @@ class Adapter :
             .into(holder.image)
 
         holder.itemView.setOnClickListener {
-            Toast.makeText(holder.itemView.context, "Item Clicked", Toast.LENGTH_SHORT).show()
+            Constant.userData = currentItem
+            val intent = Intent(holder.itemView.context, DetailedActivity::class.java)
+            holder.itemView.context.startActivity(intent)
         }
 
     }
---
+++ app/src/main/java/com/example/diffutilsrecyclerview/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.diffutilsrecyclerview
+
+import com.example.diffutilsrecyclerview.model.User
+
+object Constant {
+    var userData: User? = null
+}
